945b2aaac54c4353312891c3dec673a7557f9529
hyecheon <rainbow880616@gmail.com>
2019-09-19 01:12:28 +0900

    chapter04

---
+++ chapter04/src/main/kotlin/com/hyecheon/section03/CustomExceptionTest.kt
@@ -0,0 +1,22 @@
+package com.hyecheon.section03
+
+import java.lang.Exception
+
+class InvalidNameException(message: String) : Exception(message)
+
+fun main() {
+    var name = "Kildong123"
+    try {
+        validateName(name)
+    } catch (e: InvalidNameException) {
+        println(e.message)
+    } catch (e: Exception) {
+        println(e.message)
+    }
+}
+
+fun validateName(name: String) {
+    if (name.matches(Regex(".*\\d+.*"))) {
+        throw InvalidNameException("Your name : $name : contains numbers")
+    }
+}
